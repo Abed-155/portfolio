f9a9378c96ae2b90939243039d2bfa3d152d6263
Abed Ali <mdabedali15@gmail.com>
2023-12-03 22:39:20 +0600

    some updae on blog pase

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,18 +1,17 @@
 import Layout from "../components/Layout";
-import Form from "../components/contact/Form";
+import HeadTitle from "../components/utility/HeadTitle";
 
 export default function Blog() {
   return (
     <>
       <Layout>
-        <h1>Blog Page</h1>
+        <HeadTitle label="Blogs" />
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. A blanditiis
           explicabo asperiores! Soluta voluptates, voluptatem, quam ratione
           debitis dolores excepturi deserunt nihil officia laudantium ipsum?
           Accusantium fuga facere reiciendis in?
         </p>
-        <Form />
       </Layout>
     </>
   );
